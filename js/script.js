a7bdefb7e60f4ca4109598e9752307c0fe37db91
Dima <dimfreelancerdn@gmail.com>
2021-01-09 08:27:37 +0300

    bonus простая разметка прокта получены button кнопки

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@
  * Бонусное задание выходного дня
  */
 
-alert('hello');
+const registerBtn = document.getElementById('register-user');
+console.log('registerUserBtn: ', registerBtn);
+const loginBtn = document.getElementById('login');
+console.log(' loginBtn: ',  loginBtn);
+
+
+
+
+
+
+ alert('hello');
+
