98a15f1bcb23807c303de078c9db238e8d02da1b
Dima <dimfreelancerdn@gmail.com>
2021-01-09 08:46:16 +0300

    bonus навесили eventListener на кнопки button

--- js/script.js
+++ js/script.js
@@ -3,14 +3,47 @@
  * Бонусное задание выходного дня
  */
 
+//получим кнопки элементы
 const registerBtn = document.getElementById('register-user');
-console.log('registerUserBtn: ', registerBtn);
 const loginBtn = document.getElementById('login');
-console.log(' loginBtn: ',  loginBtn);
+
+// console.log('registerUserBtn: ', registerBtn);
+// console.log(' loginBtn: ',  loginBtn);
+
+//массив данных приложения
+let appData = [];
+
+/** TODO чтение данных пользователя из localStorage  */
+const KEY = 'key';
 
 
 
+//слушатель на кнопку регистрации
+registerBtn.addEventListener('click', (event) => {
 
+    let answer = prompt('Введите через пробел Имя Фамилию пользователя');
+
+    /** TODO сделать проверку на Имя Фамилию через пробел */
+    alert('Вы ввели ' + answer);
+
+
+    /** TODO записываем данные пользователя в localStorage */
+});
+
+
+//продолжение слушатель на кнопку авторизации пользователя
+loginBtn.addEventListener('click', () => {
+
+    let login = prompt('Введите одно слово Логин пользователя');
+    console.log('login: ', login);
+    
+    let password = prompt('Введите одно слово Пароль пользователя');
+    console.log('password: ', password);
+    
+    /** TODO проверка на наличие пользователя */
+
+    console.log('Авторизация пользователя');
+});
 
 
  alert('hello');
